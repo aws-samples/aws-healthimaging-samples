0433604ab7e0ac719fa2eae6cabf244b2d64e5d6
Chris <521621+ctpr@users.noreply.github.com>
2023-08-03 08:15:40 +0200

    Update selectOptions.js - enabling different region options

--- imaging-viewer-ui/src/components/Settings/selectOptions.js
+++ imaging-viewer-ui/src/components/Settings/selectOptions.js
@@ -1,8 +1,8 @@
 const appRegionOptions = [
     { label: 'US East (N. Virginia)', value: 'us-east-1' },
-    { label: 'US West (Oregon)', value: 'us-west-2', disabled: true },
-    { label: 'Europe (Ireland)', value: 'eu-west-1', disabled: true },
-    { label: 'Asia Pacific (Sydney)', value: 'ap-southeast-2', disabled: true },
+    { label: 'US West (Oregon)', value: 'us-west-2', disabled: false },
+    { label: 'Europe (Ireland)', value: 'eu-west-1', disabled: false },
+    { label: 'Asia Pacific (Sydney)', value: 'ap-southeast-2', disabled: false },
 ];
 
 const tlmAuthOptions = [
