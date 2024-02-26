16e4afdd275167677337ef632068300820a56fa4
Chris Hafey <cehafey@amazon.com>
2024-02-26 17:29:00 -0600

    cleaned up build.sh

--- ahi-batch-image-frame-retrieve/build.sh
+++ ahi-batch-image-frame-retrieve/build.sh
@@ -9,16 +9,19 @@ rm -rf imageFrames/*
 
 rm -rf build/* 
 
-PKG_CONFIG_PATH="/opt/homebrew/opt/curl/lib/pkgconfig"
+# BEGIN Mac OS X Homebrew config
+export PKG_CONFIG_PATH="/opt/homebrew/opt/curl/lib/pkgconfig" # allows cmake to find curl in homebrew directory on mac os x since it is installed as a keg to avoid conflicts with the system libcurl
+export CMAKE_PREFIX_PATH="/opt/homebrew" # allows cmake to find packages installed via homebrew
+# END Mac OS X Homebrew config
+
+export CMAKE_BUILD_TYPE=Debug
 
 # use cmake to generate the makefile
 if [ $(arch) = 'x86_64' ]
 then
-  (cd build && cmake -DCMAKE_BUILD_TYPE=Release -DOJPH_DISABLE_INTEL_SIMD=OFF ..)
+  (cd build && cmake  -DOJPH_DISABLE_INTEL_SIMD=OFF ..)
 else
-  # Mac OS X
-  #(cd build && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/opt/homebrew -DCURL_DIR=/opt/homebrew/opt/curl/shared/cmake -DOJPH_DISABLE_INTEL_SIMD=ON ..)
-  (cd build && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=/opt/homebrew -DCURL_DIR=/opt/homebrew/opt/curl/shared/cmake -DOJPH_DISABLE_INTEL_SIMD=ON ..)
+  (cd build && cmake -DOJPH_DISABLE_INTEL_SIMD=ON ..)
 fi
 
 # Build it
