bd42b100b5e1126930619113ebd73399053a1b71
Andrew <andrew@openmarmot.com>
2024-05-14 14:02:39 -0700

    s3storescp updates (#65)
    * Update s3storescp.py
    
    fixing aws-cdk environment function calls
    
    * Update requirements.txt
    
    update aws-cdk-lib version

--- s3-storescp/s3storescp.py
+++ s3-storescp/s3storescp.py
@@ -6,6 +6,7 @@ from aws_cdk import (
     App,
     CfnOutput,
     Duration,
+    Environment,
     Stack,
     RemovalPolicy,
     aws_ec2 as ec2,
@@ -241,7 +242,7 @@ S3StoreSCPStack(
     app, 
     cfg.APP_NAME, 
     description=cfg.CFN_STACK_DESCRIPTION, 
-    env={'region':cfg.DEPLOY_REGION}
+    #env={'region':cfg.DEPLOY_REGION}
     
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
@@ -250,12 +251,12 @@ S3StoreSCPStack(
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    #env=Environment(account='123456789012', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
